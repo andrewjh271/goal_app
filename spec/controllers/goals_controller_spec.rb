2b83e99a5aa30235e4d3fa6786cece0d61891b89
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2020-09-25 21:31:42 -0700

    Changed from using before(:all) to using let

--- spec/controllers/goals_controller_spec.rb
+++ spec/controllers/goals_controller_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe GoalsController, type: :controller do
-  before(:all) { @user = User.create!(username: 'cheetah', password: 'jungle') }
-  after(:all) { @user.destroy }
+  # before(:all) { @user = User.create!(username: 'cheetah', password: 'jungle') }
+  # after(:all) { @user.destroy }
+  let(:user) { User.create!(username: 'cheetah', password: 'jungle') }
+  let(:goal) { FactoryBot.create(:goal, user_id: user.id) }
 
   describe 'GET #new' do
     it 'renders the new template' do
-      login_user(@user)
+      login_user(user)
       get :new
       expect(response).to render_template(:new)
     end
@@ -20,7 +22,7 @@ RSpec.describe GoalsController, type: :controller do
       end
 
       it 'validates the presence of title' do
-        login_user(@user)
+        login_user(user)
         post :create, params: { goal: { nonsense: true } }
         expect(response).to render_template(:new)
         expect(flash[:errors]).to eq(['Title can\'t be blank'])
@@ -28,34 +30,34 @@ RSpec.describe GoalsController, type: :controller do
     end
     context 'with valid params' do
       it 'redirects to user show page on success' do
-        login_user(@user)
+        login_user(user)
         post :create, params: { goal: { title: 'Eat a raspberry' } }
-        expect(response).to redirect_to(user_url(@user.id))
+        expect(response).to redirect_to(user_url(user.id))
       end
     end
   end
 
   describe 'GET #edit' do
     it 'renders the edit template' do
-      login_user(@user)
-      goal = FactoryBot.create(:goal, user_id: @user.id)
+      login_user(user)
+      goal = FactoryBot.create(:goal, user_id: user.id)
       get :edit, params: { id: goal.id }
       expect(response).to render_template(:edit)
     end
   end
 
   describe 'POST #update' do
-    before(:each) { @goal = FactoryBot.create(:goal, user_id: @user.id) }
+    # before(:each) { @goal = FactoryBot.create(:goal, user_id: @user.id) }
 
     context 'with invalid params' do
       it 'requires current_user' do
-        post :update, params: { goal: { title: 'Eat a raspberry' }, id: @goal.id }
+        post :update, params: { goal: { title: 'Eat a raspberry' }, id: goal.id }
         expect(response).to redirect_to(new_session_url)
       end
 
       it 'validates the presence of title' do
-        login_user(@user)
-        post :update, params: { goal: { title: nil }, id: @goal.id }
+        login_user(user)
+        post :update, params: { goal: { title: nil }, id: goal.id }
         expect(response).to render_template(:edit)
         expect(flash[:errors]).to eq(['Title can\'t be blank'])
       end
@@ -63,21 +65,21 @@ RSpec.describe GoalsController, type: :controller do
 
     context 'with valid params' do
       it 'redirects to user show page on success' do
-        login_user(@user)
-        post :update, params: { goal: { title: 'Eat a raspberry' }, id: @goal.id }
-        expect(response).to redirect_to(user_url(@user.id))
+        login_user(user)
+        post :update, params: { goal: { title: 'Eat a raspberry' }, id: goal.id }
+        expect(response).to redirect_to(user_url(user.id))
       end
     end
   end
 
   describe 'DELETE #destroy' do
-    before(:each) { @goal = FactoryBot.create(:goal, user_id: @user.id) }
+    # before(:each) { @goal = FactoryBot.create(:goal, user_id: @user.id) }
 
     it 'deletes a goal and redirects to user\'s show page' do
-      login_user(@user)
-      delete :destroy, params: { id: @goal.id }
+      login_user(user)
+      delete :destroy, params: { id: goal.id }
       expect(response).to redirect_to(user_url)
-      expect(Goal.find_by(id: @goal.id)).to be nil
+      expect(Goal.find_by(id: goal.id)).to be nil
     end
   end
 
--- spec/features/comment_spec.rb
+++ spec/features/comment_spec.rb
@@ -1,45 +1,39 @@
 require 'rails_helper'
 
 feature 'user comments' do
-  before(:all) do
-    @user= FactoryBot.create(:user)
-    @goal = FactoryBot.create(:goal)
-  end
 
-  after(:all) do
-    @goal.user.destroy
-    @user.destroy
-  end
+  given(:user) { FactoryBot.create(:user) }
+  given(:goal) { FactoryBot.create(:goal) }
 
   feature 'creating a comment on a user' do
     scenario 'user page has a comment input' do
-      login(@user)
-      visit user_url(@user.id)
+      login(user)
+      visit user_url(user.id)
       expect(page).to have_button 'Post Comment'
     end
 
     scenario 'user can comment on a user page' do
-      login(@user)
-      visit user_url(@user.id)
+      login(user)
+      visit user_url(user.id)
       fill_in 'body', with: 'Good luck'
       click_on 'Post Comment'
       expect(page).to have_content 'Good luck'
-      expect(page).to have_content "—#{@user.username}"
+      expect(page).to have_content "—#{user.username}"
     end
   end
 
   feature 'deleting a comment on a user' do
     scenario 'user can delete comments on their page' do
-      comment = FactoryBot.create(:comment, subject: @user)
-      login(@user)
-      visit user_url(@user.id)
+      comment = FactoryBot.create(:comment, subject: user)
+      login(user)
+      visit user_url(user.id)
       expect(page).to have_content comment.body
       click_on 'Delete Comment'
       expect(page).not_to have_content comment.body
     end
 
     scenario 'author can delete their comments' do
-      comment = FactoryBot.create(:comment, author: @user)
+      comment = FactoryBot.create(:comment, author: user)
       login(comment.subject)
       visit user_url(comment.subject)
       expect(page).to have_content comment.body
@@ -50,55 +44,58 @@ feature 'user comments' do
 end
 
 feature 'goal comments' do
-  before(:all) do
-    @user= FactoryBot.create(:user)
-    @goal = FactoryBot.create(:goal)
-  end
+  # before(:all) do
+  #   @user= FactoryBot.create(:user)
+  #   @goal = FactoryBot.create(:goal)
+  # end
 
-  after(:all) do
-    @goal.user.destroy
-    @user.destroy
-  end
+  # after(:all) do
+  #   @goal.user.destroy
+  #   @user.destroy
+  # end
+
+  given(:user) { FactoryBot.create(:user) }
+  given(:goal) { FactoryBot.create(:goal) }
 
   feature 'creating a comment on a goal' do
     scenario 'goal page has comment input' do
-      visit goal_url(@goal)
+      visit goal_url(goal)
       expect(page).to have_button 'Post Comment'
     end
 
     scenario 'user can comment on a goal page' do
-      login(@user)
-      visit goal_url(@goal)
+      login(user)
+      visit goal_url(goal)
       fill_in 'body', with: 'Wow, nice goal.'
       click_on 'Post Comment'
       expect(page).to have_content 'Wow, nice goal.'
-      expect(page).to have_content "—#{@user.username}"
+      expect(page).to have_content "—#{user.username}"
     end
   end
 
   feature 'deleting a comment on a user' do
     scenario 'creator of goal can delete its comments' do
-      comment = FactoryBot.create(:comment, subject: @goal)
+      comment = FactoryBot.create(:comment, subject: goal)
       login(comment.author)
-      visit goal_url(@goal)
+      visit goal_url(goal)
       expect(page).to have_content comment.body
       click_on 'Delete Comment'
       expect(page).not_to have_content comment.body
     end
 
     scenario 'author of comment can delete it' do
-      comment = FactoryBot.create(:comment, subject: @goal, author: @user)
-      login(@user)
-      visit goal_url(@goal)
+      comment = FactoryBot.create(:comment, subject: goal, author: user)
+      login(user)
+      visit goal_url(goal)
       expect(page).to have_content comment.body
       click_on 'Delete Comment'
       expect(page).not_to have_content comment.body
     end
 
     scenario 'random user cannot delete comment' do
-      comment = FactoryBot.create(:comment, subject: @goal)
-      login(@user)
-      visit goal_url(@goal)
+      comment = FactoryBot.create(:comment, subject: goal)
+      login(user)
+      visit goal_url(goal)
       expect(page).not_to have_button 'Delete Comment'
     end
   end
