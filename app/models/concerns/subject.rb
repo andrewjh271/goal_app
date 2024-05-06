b0c404978452e026a0fc8ce4f68f03d8154f099a
Andrew Hayhurst <hayhurst.andrew@gmail.com>
2024-05-05 23:03:26 -0700

    Add explanation to Subject concern

--- app/models/concerns/subject.rb
+++ app/models/concerns/subject.rb
@@ -2,6 +2,8 @@
 # it would have been (especially once using a Concern) to say Commentable
 # Next time.
 
+# https://dev.to/software_writer/how-rails-concerns-work-and-how-to-use-them-gi6
+
 module Subject extend ActiveSupport::Concern
   included do
     has_many :comments, as: :subject
